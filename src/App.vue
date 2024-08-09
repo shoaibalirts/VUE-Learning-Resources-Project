00acb6f4046eccd84338c5dd37050ba4727cdf38
shoaibalirts <shoaibali.rts@gmail.com>
2024-08-09 11:50:10 +0200

    kept app component a bit cleaner and therefore added other vue components e.g. StoredResources

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <stored-resources :stored-resources="storedResources"></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/StoredResources.vue';
+
+export default {
+  components: {
+    StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'Official Guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://learning.google/',
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/StoredResources.vue
@@ -0,0 +1,28 @@
+<template>
+  <ul v-for="resource in storedResources" :key="resource.id">
+    <learning-resource
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+<script>
+import LearningResource from './LearningResource.vue';
+export default {
+  components: {
+    LearningResource,
+  },
+  props: ['storedResources'],
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
